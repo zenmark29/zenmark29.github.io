264a1acc3c207269af9da593b10c4c9be8a713a5
Robert Mark Mills <zenmark29@users.noreply.github.com>
2017-05-05 21:31:09 -0500

    "update"

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 function startTimer(duration, display) {
     var start = Date.now(),
         diff,
-        days, 
+        days,
         hours,
         minutes,
         seconds,
-        intervalID, 
+        intervalID,
         schoolIsIn = true;
+
     function timer() {
         // get the number of seconds that have elapsed since 
         // startTimer() was called
         diff = duration - (((Date.now() - start) / 1000) | 0);
 
         if (diff <= 0 && schoolIsIn) {
-          clearInterval(intervalID);
-          document.getElementById("mybody").textContent  ="school is out!";
-          document.getElementById("myLink").innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/2Oo8QzDHimQ?autoplay=1" frameborder="0" allowfullscreen></iframe>';
-          schoolIsIn = false;
-          return;
+            clearInterval(intervalID);
+            document.getElementById("mybody").textContent = "school is out!";
+            document.getElementById("myLink").innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/2Oo8QzDHimQ?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            schoolIsIn = false;
+            return;
         }
         // does the same job as parseInt truncates the float
-        
-        days  = Math.floor(diff / 86400) | 0;
-       
-        hours = Math.floor((diff % 86400)/3600) | 0;
-        
-        
-        minutes = Math.floor((diff % 3600)/60) | 0;
+
+        days = Math.floor(diff / 86400) | 0;
+
+        hours = Math.floor((diff % 86400) / 3600) | 0;
+
+
+        minutes = Math.floor((diff % 3600) / 60) | 0;
         seconds = (diff % 60) | 0;
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent =  days + " days " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds until freedom!"; 
+        display.textContent = days + " days " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds until freedom!";
 
         if (diff <= 0) {
             // add one second so that the count down starts at the full duration
@@ -41,22 +42,22 @@ function startTimer(duration, display) {
         }
     }
     // we don't want to wait a full second before the timer starts
-    if (schoolIsIn){
-      timer();
-      intervalID = setInterval(timer, 1000);
+    if (schoolIsIn) {
+        timer();
+        intervalID = setInterval(timer, 1000);
     }
 }
 
 
 
 
-window.onload = function () {
+window.onload = function() {
     var today = new Date();
-    var lastDay = new Date(2015, 5, 4, 15, 30, 0);
+    var lastDay = new Date(2017, 5, 1, 15, 30, 0);
     //var lastDay = new Date(2015, 4, 20, 21, 56, 0);
     var lastDayString = lastDay.toDateString();
-    var duration = (lastDay - today)/(1000);
-    
+    var duration = (lastDay - today) / (1000);
+
     display = document.getElementById("time_remaining");
     startTimer(duration, display);
 };
