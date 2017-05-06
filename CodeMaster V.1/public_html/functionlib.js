0783c67515fdc146d152f32a471031b499179c03
Robert Mark Mills <zenmark29@users.noreply.github.com>
2017-05-06 14:58:34 -0500

    update the copyright

--- CodeMaster V.1/public_html/functionlib.js
+++ CodeMaster V.1/public_html/functionlib.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Robert Mark Mills - All Rights Reserved
+/* Copyright (C) 2017 Robert Mark Mills - All Rights Reserved
  You may not use, distribute and/or modify this code without the express, 
  written consent of the owner named above. 
  For information email: robert.mark.mills@gmail.com
