1e0c935e31b661d44c90147d5fce1df55a052fb7
Mark Mills <robert.mark.mills@gmail.com>
2018-11-25 17:40:49 -0600

    more junk

--- FunctionCreator/src/FunctionCreator.js
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2016 Robert Mark Mills - All Rights Reserved
- You may not use, distribute and/or modify this code without the express, 
- written consent of the owner named above. 
- For information email: robert.mark.mills@gmail.com
- */
-
-
-
